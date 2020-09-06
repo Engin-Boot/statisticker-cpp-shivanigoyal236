554e1aae349a7229f999f23751bb5a2fcc2933d0
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-09-06 23:13:45 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,22 +2,23 @@
 
 #include "catch.hpp"
 #include "stats.h"
-
+#include <math.h>
 #include <cmath>
 
 TEST_CASE("reports average, minimum and maximum") {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    auto computedStats = Statistics::ComputeStatistics({1.5f, 8.9f, 3.2f, 4.5f});
     float epsilon = 0.001;
-    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.avg - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the REQUIRE statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
+    REQUIRE(isnan(computedStats.avg));
+}
+
+TEST_CASE("reports average for a array having nan values") {
+    auto computedStats = Statistics::ComputeStatistics({1.5f,8.9f,3.2f,4.5f,NAN,NAN});
+    REQUIRE(isnan(computedStats.avg));
 }
