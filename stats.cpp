cafd5d03741bbc8fa721d6d07e7cd8fe276d98fe
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-09-06 23:13:10 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,12 @@
 #include "stats.h"
+#include<cmath>
+#include<algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& sample) {
     //Implement statistics here
-}
+    Stats statistics;
+	statistics.ComputeAverage(sample);
+	statistics.ComputeMax(sample);
+	statistics.ComputeMin(sample);
+	return statistics;
+};
